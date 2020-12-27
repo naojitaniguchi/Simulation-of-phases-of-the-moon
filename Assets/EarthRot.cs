2a44b41c05b379741db3820f98c52b1ff3fefaf9
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-12-27 09:56:14 +0900

    First commit.

---
+++ Assets/EarthRot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EarthRot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void rotBySlider(Slider slider)
+    {
+        gameObject.transform.rotation = Quaternion.AngleAxis(slider.value * -360.0f, Vector3.up);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Simulation of phases of the moon
+[![youtube](https://img.youtube.com/vi/0RdMMjj1s4A/0.jpg)](http://www.youtube.com/watch?v=0RdMMjj1s4A "Simulation of phases of the moon")
